d8cbb387ee6fc2dbed2847f8ee34b2e537d67f54
Christopher McKee <chris.mckee.13@gmail.com>
2020-01-25 12:50:15 -0800

    adds heroku sql connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burger_db"
+// });
+
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "uc13jynhmkss3nve.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "root",
-  database: "burger_db"
+  user: "c92pltefcm30hw4m",
+  password: "zlddyge46r7po434",
+  database: "ahowj90x5125wfk3"
 });
 
 // Make connection.
