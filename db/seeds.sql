5bf78c38003317e924ceb1989cd7df0e81395831
Christopher McKee <chris.mckee.13@gmail.com>
2020-01-25 10:00:36 -0800

    changes references from cats to burgers; not currently functional

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,13 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Cheese Burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Bacon Burger');
+INSERT INTO burgers
+    (name, eaten)
+VALUES
+    ('Impossible Burger', true);
+
