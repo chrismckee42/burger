6c4b7ba30cc8f09c9355433b1f84159023eb046e
Christopher McKee <chris.mckee.13@gmail.com>
2020-01-25 12:30:33 -0800

    fixes problem with SQL

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -19,9 +19,9 @@ router.get("/", function (req, res) {
 router.post("/api/burgers", function (req, res) {
   console.log(req.body)
   burger.create([
-    "name", "eaten"
+    "name"
   ], [
-    req.body.name, req.body.eaten
+    req.body.name
   ], function (result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
